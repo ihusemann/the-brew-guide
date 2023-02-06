d1d92117bcbed65b18b5347dfaa9da4b9c900519
Isaac Husemann <isaac.husemann@gmail.com>
2023-02-06 16:56:41 -0600

    updated header opacity animation to only affect the shadow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,12 +15,12 @@ const Header = ({ children }: Props) => {
   });
   return (
     <>
-      <motion.div className="fixed inset-y-0 top-0 z-50 h-20 w-full">
+      <div className="fixed inset-y-0 top-0 z-50 h-20 w-full">
         <motion.div
           style={{ opacity: scrollYProgress }}
-          className="absolute inset-0 bg-white shadow-md"
-        />
-        <div className="absolute inset-0 mx-auto flex max-w-5xl items-center justify-between px-10 py-4">
+          className="absolute inset-0 shadow-md"
+        ></motion.div>
+        <div className="absolute inset-0 mx-auto flex max-w-5xl items-center justify-between bg-white px-10 py-4">
           <a
             href="/"
             className="text-3xl font-black tracking-tighter text-neutral-900"
@@ -32,7 +32,7 @@ const Header = ({ children }: Props) => {
             {children}
           </div>
         </div>
-      </motion.div>
+      </div>
     </>
   );
 };
