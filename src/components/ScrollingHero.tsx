a37c9f6c13dc8c7b7b5b9305e4f64206ed43b23c
Isaac Husemann <isaac.husemann@gmail.com>
2023-02-10 17:05:28 -0600

    updated size and scroll speed of scrolling hero

--- src/components/ScrollingHero.tsx
+++ src/components/ScrollingHero.tsx
@@ -6,14 +6,12 @@ const ScrollingHero = () => {
     <div className="py-12">
       <motion.h1
         aria-hidden="true"
-        animate={{ x: "-100%" }}
-        transition={{ duration: 55, ease: "linear" }}
-        initial={{ x: "-100px" }}
-        className="text-outline whitespace-nowrap stroke-black text-[12rem] font-bold uppercase tracking-wider text-transparent"
+        animate={{ x: "-1000px" }}
+        transition={{ duration: 35, ease: "linear", repeat: Infinity }}
+        className="text-outline whitespace-nowrap stroke-black text-7xl sm:text-9xl lg:text-[12rem] font-bold uppercase tracking-wider text-transparent"
       >
         BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES
-        BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES
-        BREW GUIDES BREW GUIDES
+        BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES
       </motion.h1>
       <h1 className="sr-only text-center text-7xl font-bold">Brew Guides</h1>
     </div>
