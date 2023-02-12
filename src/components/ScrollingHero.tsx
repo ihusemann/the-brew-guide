1418d93b2e2b608c4f5b1e74d0d7b0afd630ba24
Isaac Husemann <isaac.husemann@gmail.com>
2023-02-12 11:02:13 -0600

    added label prop for scrolling header

--- src/components/ScrollingHero.tsx
+++ src/components/ScrollingHero.tsx
@@ -1,19 +1,23 @@
 import { motion } from "framer-motion";
 import "src/components/ScrollingHero.css";
 
-const ScrollingHero = () => {
+interface Props {
+  label: string;
+  numRepeat?: number;
+}
+
+const ScrollingHero = ({ label, numRepeat = 10 }: Props) => {
   return (
-    <div className="py-12 overflow-x-hidden">
+    <div className="overflow-x-hidden py-12">
       <motion.h1
         aria-hidden="true"
         animate={{ x: "-1000px" }}
         transition={{ duration: 35, ease: "linear", repeat: Infinity }}
-        className="text-outline whitespace-nowrap stroke-black text-7xl sm:text-9xl lg:text-[12rem] font-bold uppercase tracking-wider text-transparent"
+        className="text-outline whitespace-nowrap stroke-black text-7xl font-bold uppercase tracking-wider text-transparent sm:text-9xl lg:text-[12rem]"
       >
-        BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES
-        BREW GUIDES BREW GUIDES BREW GUIDES BREW GUIDES
+        {`${label} `.repeat(numRepeat)}
       </motion.h1>
-      <h1 className="sr-only text-center text-7xl font-bold">Brew Guides</h1>
+      <h1 className="sr-only text-center text-7xl font-bold">{label}</h1>
     </div>
   );
 };
