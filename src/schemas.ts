465f939dc1de5fcadc810552e6351575fc6cfe35
Isaac Husemann <isaac.husemann@gmail.com>
2023-02-15 07:59:34 -0600

    updated content schema to be more reusable

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,23 +1,35 @@
 import { z } from "astro:content";
 
+const author = z.object({
+  name: z.string(),
+  slug: z.string(),
+});
+
+const image = z.object({
+  src: z.string(),
+  alt: z.string(),
+});
+
+const tags = z.array(z.string());
+
 export const guideSchema = z.object({
   isDraft: z.boolean(),
   title: z.string(),
   subtitle: z.string(),
   description: z.string(),
-  author: z.object({
-    name: z.string(), 
-    slug: z.string(),
-  }),
+  author,
   pubDate: z.date(),
-  tags: z.array(z.string()),
-  image: z.object({
-    src: z.string(),
-    alt: z.string(),
-  }),
-  equipment: z.array(
-    z.object({
-      name: z.string(),
-    })
-  ),
+  tags,
+  image,
+});
+
+export const gearSchema = z.object({
+  isDraft: z.boolean(),
+  title: z.string(),
+  subtitle: z.string(),
+  description: z.string(),
+  author,
+  pubDate: z.date(),
+  image,
+  tags,
 });
