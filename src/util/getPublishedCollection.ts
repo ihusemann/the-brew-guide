5ed1e4adb1dbb1c511fd2456af91621aa14e4c96
Isaac Husemann <isaac.husemann@gmail.com>
2023-02-16 16:10:09 -0600

    updated getPublishedPosts to include unpublished posts in development

--- src/util/getPublishedCollection.ts
+++ src/util/getPublishedCollection.ts
@@ -1,13 +1,16 @@
 import { getCollection } from "astro:content";
 
 /**
- * Returns all published posts.  Optionally, returns the posts including a specific tag. 
+ * Returns all published posts.  Optionally, returns the posts including a specific tag.
  */
 export const getPublishedPosts = async (tag?: string) => {
   const entries = await getCollection("posts");
 
-  // getCollection() should be able to natively do this filtering, but throws a type error.  Workaround for now.
-  const published = entries.filter(({ data }) => !data.isDraft);
+  // filter out the draft posts in non-development environments.  getCollection() should be able to natively do this type of filtering, but throws a type error.  Workaround for now.
+  const published =
+    import.meta.env.ENVIRONMENT !== "development"
+      ? entries.filter(({ data }) => !data.isDraft)
+      : entries;
 
   return !tag
     ? published
