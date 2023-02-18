b28a5b7294ce29598ca498461e6e4a356ef05137
Isaac Husemann <isaac.husemann@gmail.com>
2023-02-18 13:01:02 -0600

    fixed rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,21 @@
 import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
-import { getCollection } from "astro:content";
+import { getPublishedPosts } from "src/util";
 
 export async function get(context: APIContext) {
-  const guides = await getCollection("guides");
+  const posts = await getPublishedPosts();
   return rss({
     title: "The Brew Guide",
     description:
       "Brew up better coffee at home!  The Brew Guide has everything you need to get up-and-running with exceptional coffee and espresso.",
     site: context.site as unknown as string,
-    items: guides.map((guide) => ({
-      title: guide.data.title,
-      pubDate: guide.data.pubDate,
-      description: guide.data.description,
-      link: `/guides/${guide.slug}`,
-    })),
+    items: posts.map(
+      ({ slug, data: { title, subtitle, description, pubDate } }) => ({
+        title: title + subtitle,
+        description: description,
+        pubDate: pubDate,
+        link: `/${slug}`,
+      })
+    ),
   });
 }
